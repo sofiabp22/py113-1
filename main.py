fc1744d0b5db9d00abe7ab8763a8547641b59330
Sofia Pereira <76704668+sofiabp22@users.noreply.github.com>
2021-12-12 13:54:17 -0500

    Refactor jokes

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ import random # standard python random library
 
 app = FastAPI() # this creates ours FastAPI application
 
+JOKES = [
+    {
+       "question": "Where do fruits go on vacation?",
+        "answer": "Pear-is!"
+    },
+    {
+        "question": "What do you call a fish wearing a bowtie?",
+        "answer": "Sofishticated."
+    },
+    {
+        "question": "What did the ocean say to the beach?",
+        "answer:": "Nothing, it just waved."
+    }
+]
+
+
 # this defines our root endpoint
 @app.get("/")
 async def root():
@@ -12,25 +28,13 @@ async def root():
 # this endpoint returns a random joke
 @app.get("/random") 
 async def random_joke():
-    jokes = [
-        {
-            "question": "What do you call a fish wearing a bowtie?",
-            "answer": "Sofishticated."
-        },
-        {
-            "question": "What did the ocean say to the beach?",
-            "answer:": "Nothing, it just waved."
-        }
-    ]
-    return random.choice(jokes)
+    return random.choice(JOKES)
 
 
 @app.get("/joke-of-the-year") 
 async def joke_of_the_year():
-    joke = {
-            "question": "Where do fruits go on vacation?",
-            "answer": "Pear-is!"
-        }
-    return joke
+    return JOKES[0]
 
-    
+@app.get("/all-jokes") 
+async def all_jokes():
+    return JOKES
